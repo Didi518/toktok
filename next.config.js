00333378569ceb0ebe4a4912b5749c237aa94d4d
Didi518 <kevin.ouali@gmail.com>
2022-07-16 23:47:57 +0200

    mise en ligne

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
   reactStrictMode: true,
   images: {
     domains: ['th.bing.com', 'lh3.googleusercontent.com'],
