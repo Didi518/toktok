b89b57939d47850a0b2eda68a1632c1bf4f0a6db
Didi518 <kevin.ouali@gmail.com>
2022-07-16 19:13:46 +0200

    setup des commentaires

--- components/NoResults.tsx
+++ components/NoResults.tsx
@@ -1,11 +1,24 @@
 import React from 'react';
+import { MdOutlineVideocamOff } from 'react-icons/md';
+import { BiCommentX } from 'react-icons/bi';
 
 interface Iprops {
   text: string;
 }
 
 const NoResults = ({ text }: Iprops) => {
-  return <div></div>;
+  return (
+    <div className="flex flex-col justify-center items-center h-full w-full">
+      <p className="text-8xl">
+        {text === 'Pas encore de commentaire' ? (
+          <BiCommentX />
+        ) : (
+          <MdOutlineVideocamOff />
+        )}
+      </p>
+      <p className="text-2xl text-center">{text}</p>
+    </div>
+  );
 };
 
 export default NoResults;
